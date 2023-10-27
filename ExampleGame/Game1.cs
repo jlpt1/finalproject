a90e0736a62677c2b021ef0ed71e829e2066d7c3
kcjan <kcjan@JLPT>
2023-10-27 03:58:30 -0500

    gdsa

--- ExampleGame/Game1.cs
+++ ExampleGame/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.IO;
+using System.Text;
 
 namespace ExampleGame
 {
@@ -19,20 +22,51 @@ namespace ExampleGame
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
         }
 
         protected override void Initialize()
         {
+            File.WriteAllText("../Fuck.txt", "You");
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _tilemap = Content.Load<BasicTilemap>("example");
+            string currentDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string projectRoot = Directory.GetParent(Directory.GetParent(currentDirectory).FullName).FullName;
+            string contentDirectory = Path.Combine(projectRoot, "Content");
 
-            // Loading the tilemap
+            try
+            {
+                // Modify the tilemap content
+                string fakepath = Path.Combine(Content.RootDirectory, "example.tmap");
+                string tilemapPath = Path.Combine(contentDirectory, "example.tmap");
+                if (File.Exists(fakepath))
+                {
+                    string content = File.ReadAllText(tilemapPath, Encoding.UTF8);
+                    content = content.Replace("4", "3");
+                    File.WriteAllText(tilemapPath, content, Encoding.UTF8);
+                }
+
+                // Load the modified tilemap
+                _tilemap = Content.Load<BasicTilemap>("example");
+            }
+            catch (Exception ex)
+            {
+                // Log the error to a file
+                File.AppendAllText("log.txt", $"Error during tilemap processing: {ex.Message}\n");
+            }
             _tilemap = Content.Load<BasicTilemap>("example");
         }
+        
+
+
+ 
+
+        
 
         protected override void Update(GameTime gameTime)
         {
